b156b216b5e28fc51f2842e270c3e8ecd3eb5f62
Gang Deng <13918383742@163.com>
2023-02-24 16:38:20 +0800

    add test flag to test after convert

--- save_model.py
+++ save_model.py
@@ -1,7 +1,13 @@
+# coding=utf-8
 import tensorflow as tf
+# 必须要有这句话，否则会报错： tensorflow.python.framework.errors_impl.NotFoundError: No algorithm worked! [Op:Conv2D]
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 from absl import app, flags, logging
 from absl.flags import FLAGS
-from core.yolov4 import YOLO, decode, filter_boxes
+from core.yolov4 import YOLO, decode, filter_boxes,test_tflite,test_tf
 import core.utils as utils
 from core.config import cfg
 
@@ -12,6 +18,8 @@ flags.DEFINE_integer('input_size', 416, 'define input size of export model')
 flags.DEFINE_float('score_thres', 0.2, 'define score threshold')
 flags.DEFINE_string('framework', 'tf', 'define what framework do you want to convert (tf, trt, tflite)')
 flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
+flags.DEFINE_boolean('test', False, 'test model after convert')
+flags.DEFINE_string('image', './data/kite.jpg', 'path to input image')
 
 def save_tf():
   STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
@@ -49,6 +57,28 @@ def save_tf():
   utils.load_weights(model, FLAGS.weights, FLAGS.model, FLAGS.tiny)
   model.summary()
   model.save(FLAGS.output)
+  
+  if FLAGS.test:
+    import cv2
+    from PIL import Image
+    input_size = FLAGS.input_size
+    image_path = FLAGS.image
+    original_image = cv2.imread(image_path)
+    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+    image_data = cv2.resize(original_image, (input_size, input_size))
+    image_data = image_data / 255.
+
+    if FLAGS.framework == 'tf':
+      pred_bbox = test_tf(image_data,input_size,model,FLAGS)
+
+    image = utils.draw_bbox(original_image, pred_bbox)
+    # image = utils.draw_bbox(image_data*255, pred_bbox)
+    image = Image.fromarray(image.astype(np.uint8))
+    image.show()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+
+    cv2.imwrite(FLAGS.output, image)
+
 
 def main(_argv):
   save_tf()
