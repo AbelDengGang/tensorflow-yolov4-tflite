ef7d990c9a11ea62922bdcafd95134591b6db138
Gang Deng <13918383742@163.com>
2023-02-24 13:52:10 +0800

    add test_tf and test_tflite function

--- detect.py
+++ detect.py
@@ -24,11 +24,112 @@ flags.DEFINE_string('output', 'result.png', 'path to output image')
 flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.25, 'score threshold')
 
+def test_tflite(image_data,input_size,model):
+
+
+    images_data = []
+    for i in range(1):
+        images_data.append(image_data)
+    images_data = np.asarray(images_data).astype(np.float32)
+
+    input_details = model.get_input_details()
+    output_details = model.get_output_details()
+    print(input_details)
+    print(output_details)
+    model.set_tensor(input_details[0]['index'], images_data)
+    model.invoke()
+    pred = [model.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+    if FLAGS.model == 'yolov3' and FLAGS.tiny == True:
+        boxes, pred_conf = filter_boxes(pred[1], pred[0], score_threshold=0.25, input_shape=tf.constant([input_size, input_size]))
+    else:
+        boxes, pred_conf = filter_boxes(pred[0], pred[1], score_threshold=0.25, input_shape=tf.constant([input_size, input_size]))
+
+    boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+        scores=tf.reshape(
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+        max_output_size_per_class=50,
+        max_total_size=50,
+        iou_threshold=FLAGS.iou,
+        score_threshold=FLAGS.score
+    )
+    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+    return pred_bbox
+
+
+def test_tf(image_data,input_size,model):
+
+    # image_data = utils.image_preprocess(np.copy(original_image), [input_size, input_size])
+    # image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+    images_data = []
+    for i in range(1):
+        images_data.append(image_data)
+    images_data = np.asarray(images_data).astype(np.float32)
+
+    if FLAGS.framework == 'tflite':
+        pass
+    else:
+        # saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+        # infer = saved_model_loaded.signatures['serving_default']
+        infer = model
+        batch_data = tf.constant(images_data)
+        pred_bbox = infer(batch_data)
+        for key, value in pred_bbox.items():
+            boxes = value[:, :, 0:4]
+            pred_conf = value[:, :, 4:]
+
+    boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+        scores=tf.reshape(
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+        max_output_size_per_class=50,
+        max_total_size=50,
+        iou_threshold=FLAGS.iou,
+        score_threshold=FLAGS.score
+    )
+    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+
+    return pred_bbox
+
+def main2(_argv):
+    # config = ConfigProto()
+    # config.gpu_options.allow_growth = True
+    # session = InteractiveSession(config=config)
+    # STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    input_size = FLAGS.size
+    image_path = FLAGS.image
+    original_image = cv2.imread(image_path)
+    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+    image_data = cv2.resize(original_image, (input_size, input_size))
+    image_data = image_data / 255.
+
+    if FLAGS.framework == 'tf':
+        saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+        infer = saved_model_loaded.signatures['serving_default']
+        pred_bbox = test_tf(image_data,input_size,infer)
+    elif FLAGS.framework == 'tflite':
+        interpreter = tf.lite.Interpreter(model_path=FLAGS.weights)
+        interpreter.allocate_tensors()
+        pred_bbox = test_tflite(image_data,input_size,interpreter)
+
+    image = utils.draw_bbox(original_image, pred_bbox)
+    # image = utils.draw_bbox(image_data*255, pred_bbox)
+    image = Image.fromarray(image.astype(np.uint8))
+    image.show()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+
+    cv2.imwrite(FLAGS.output, image)
+
+
+    return
+
+
 def main(_argv):
-    config = ConfigProto()
-    config.gpu_options.allow_growth = True
-    session = InteractiveSession(config=config)
-    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    # config = ConfigProto()
+    # config.gpu_options.allow_growth = True
+    # session = InteractiveSession(config=config)
+    # STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
     input_size = FLAGS.size
     image_path = FLAGS.image
 
@@ -87,6 +188,7 @@ def main(_argv):
 
 if __name__ == '__main__':
     try:
-        app.run(main)
+        # app.run(main)
+        app.run(main2)
     except SystemExit:
         pass
